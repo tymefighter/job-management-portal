b25e4eabed35eb56ba2ae8a8682ef9e6e437c1c0
Ahmed Zaheer Dadarkar <ahmed.dadarkar@gmail.com>
2021-07-12 14:40:12 +0530

    Add apis

---
+++ client/src/api.ts
@@ -0,0 +1,132 @@
+import * as types from "./types";
+
+const baseUrl = "http://localhost:3000";
+
+export async function fetchCompanies() {
+    const response = await fetch(baseUrl + "/companies", {
+        method: "GET"
+    });
+
+    if(!response.ok) {
+        const text = await response.text()
+        return Promise.reject(text);
+    }
+
+    const companies = await response.json();
+
+    if(types.isCompanyList(companies)) return companies;
+    else Promise.reject("company list does not have the correct type");
+}
+
+export async function addCompany(company: types.CompanyWithoutId) {
+    const response = await fetch(baseUrl + "/companies", {
+        method: "POST",
+        body: JSON.stringify(company)
+    });
+
+    const text = await response.text()
+
+    if(!response.ok) return Promise.reject(text);
+    else return text;
+}
+
+export async function fetchCompany(companyId: string) {
+    const response = await fetch(baseUrl + `/companies/${companyId}`, {
+        method: "GET"
+    });
+
+    if(!response.ok) {
+        const text = await response.text();
+        return Promise.reject(text);
+    }
+
+    const company = await response.json();
+
+    if(types.isCompany(company)) return company;
+    else Promise.reject("company does not have the correct type");
+}
+
+export async function editCompany(
+    companyId: string, companyEdit: types.CompanyEdit
+) {
+    const response = await fetch(baseUrl + `/companies/${companyId}`, {
+        method: "PUT",
+        body: JSON.stringify(companyEdit)
+    });
+
+    const text = await response.text()
+
+    if(!response.ok) return Promise.reject(text);
+    else return text;
+}
+
+export async function deleteCompany(companyId: string) {
+    const response = await fetch(baseUrl + `/companies/${companyId}`, {
+        method: "DELETE"
+    });
+
+    const text = await response.text()
+
+    if(!response.ok) return Promise.reject(text);
+    else return text;
+}
+
+export async function fetchJobs(companyId: string) {
+    const response = await fetch(baseUrl + `/companies/${companyId}/jobs`, {
+        method: "GET"
+    });
+
+    if(!response.ok) {
+        const text = await response.text();
+        return Promise.reject(text);
+    }
+
+    const jobList = await response.json();
+
+    if(types.isJobList(jobList)) return jobList;
+    else Promise.reject("job list does not have the correct type");
+}
+
+export async function addJob(companyId: string, job: types.JobWithoutId) {
+    const response = await fetch(baseUrl + `/companies/${companyId}/jobs`, {
+        method: "POST",
+        body: JSON.stringify(job)
+    });
+
+    const text = await response.text()
+
+    if(!response.ok) return Promise.reject(text);
+    else return text;
+}
+
+export async function fetchJob(
+    companyId: string, jobId: string
+) {
+    const response = await fetch(baseUrl + `/companies/${companyId}/jobs/${jobId}`, {
+        method: "GET"
+    });
+
+    if(!response.ok) {
+        const text = await response.text();
+        return Promise.reject(text);
+    }
+
+    const job = await response.json();
+
+    if(types.isJob(job)) return job;
+    else Promise.reject("job does not have the correct type");
+}
+
+export async function editJob(
+    companyId: string, jobId: string, jobEdit: types.JobEdit
+) {
+    const response = await fetch(baseUrl + `/companies/${companyId}/jobs/${jobId}`, {
+        method: "PUT",
+        body: JSON.stringify(jobEdit)
+    });
+
+    const text = await response.text()
+
+    if(!response.ok) return Promise.reject(text);
+    else return text;
+}
---
+++ client/src/types.ts
@@ -0,0 +1,168 @@
+/** Types */
+
+export interface Job {
+    id: string;
+    name: string;
+    description: string;
+    salary: number;
+    location: string;
+};
+
+export type JobWithoutId = Omit<Job, "id">;
+
+export interface Comment {
+    id: string;
+    comment: string;
+    date: string;
+};
+
+export interface Company {
+    id: string;
+    imgUrl: string;
+    name: string;
+    description: string;
+    jobs: Job[];
+    comments: Comment[]
+};
+
+export type CompanyWithoutId = Omit<Company, "id">;
+
+export interface CompanyEdit {
+    imgUrl?: string;
+    name?: string;
+    description?: string;
+};
+
+export interface JobEdit {
+    name?: string;
+    description?: string;
+    salary?: number;
+    location?: string;
+};
+
+/** Guards */
+
+function hasJobProps(job: any) {
+    if(typeof job !== "object") return false;
+
+    return (
+        typeof job.name === "string"
+        && typeof job.description === "string"
+        && typeof job.salary === "number"
+        && typeof job.location === "string"
+    );
+}
+
+export function isJob(job: any): job is Job {
+    return hasJobProps(job)
+        && typeof job.id === "string";
+}
+
+export function isJobWithoutId(job: any): job is Job {
+    return hasJobProps(job);
+}
+
+export function isJobList(jobs: any): jobs is Job[] {
+    if(!(jobs instanceof Array)) return false;
+
+    for(const job of jobs)
+        if(!isJob(job)) return false;
+    
+    return true;
+}
+
+export function isComment(comment: any): comment is Comment {
+    if(typeof comment !== "object") return false;
+
+    return (
+        typeof comment.id === "string"
+        && comment.comment === "string"
+        && comment.date === "string"
+    );
+}
+
+function hasCompanyProps(company: any) {
+    if(typeof company !== "object") return false;
+
+    if(
+        typeof company.imgUrl !== "string"
+        || typeof company.name !== "string"
+        || typeof company.description !== "string"
+        || !(company.jobs instanceof Array)
+        || !(company.comments instanceof Array)
+    ) return false;
+
+    for(const job of company.jobs)
+        if(!isJob(job)) return false;
+    
+    for(const comment of company.comments)
+        if(!isComment(comment)) return false;
+
+    return true;
+}
+
+export function isCompany(company: any): company is Company {
+    return hasCompanyProps(company)
+        && typeof company.id == "string";
+}
+
+export function isCompanyList(companies: any): companies is Company[] {
+    if(!(companies instanceof Array)) return false;
+
+    for(const company of companies)
+        if(!isCompany(company)) return false;
+    
+    return true;
+}
+
+export function isCompanyWithoutId(company: any): company is Company {
+    return hasCompanyProps(company);
+}
+
+interface AllowedPropsCompanyEditType {
+    [key: string]: string | undefined;
+    imgUrl: string;
+    name: string;
+    description: string;
+};
+
+const ALLOWED_PROPS_COMPANY_EDIT: AllowedPropsCompanyEditType = {
+    imgUrl: "string", 
+    name: "string", 
+    description: "string"
+};
+
+export function isCompanyEdit(companyEdit: any): companyEdit is CompanyEdit {
+    if(typeof companyEdit !== "object") return false;
+
+    for(const prop of Object.getOwnPropertyNames(companyEdit))
+        if(typeof companyEdit[prop] === ALLOWED_PROPS_COMPANY_EDIT[prop]) 
+            return false;
+    
+    return true;
+}
+
+interface AllowedPropsJobEditType {
+    [key: string]: string | undefined;
+    name: string;
+    description: string;
+    salary: string;
+    location: string;
+};
+
+const ALLOWED_PROPS_JOB_EDIT: AllowedPropsJobEditType = {
+    name: "string",
+    description: "string",
+    salary: "number",
+    location: "string",
+};
+
+export function isJobEdit(jobEdit: any): jobEdit is JobEdit {
+    if(typeof jobEdit !== "object") return false;
+
+    for(const prop of Object.getOwnPropertyNames(jobEdit))
+        if(typeof jobEdit[prop] === ALLOWED_PROPS_JOB_EDIT[prop]) 
+            return false;
+    
+    return true;
+}
