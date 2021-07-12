0db2c8c6e078351d87acded20449457e268674db
Ahmed Zaheer Dadarkar <ahmed.dadarkar@gmail.com>
2021-07-12 19:18:50 +0530

    Remove unused file

--- client/src/redux/stateHelper.ts
+++
@@ -1,6 +0,0 @@
-import * as dataHelper from "./dataHelper";
-import * as types from "../types";
-
-export const addCompany = dataHelper.addElement;
-export const deleteCompany = dataHelper.deleteElement;
-export const 
