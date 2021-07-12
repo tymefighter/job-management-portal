df19abd02f9457404ab1ea8269e4418d58b71f7e
Ahmed Zaheer Dadarkar <ahmed.dadarkar@gmail.com>
2021-07-12 22:24:51 +0530

    Add more tests

--- client/src/redux/__test__/dataHelper.test.ts
+++ client/src/redux/__test__/dataHelper.test.ts
@@ -88,15 +88,92 @@ test("test adding an inner element", () => {
     );
 
     expect(newArr.length).toBe(arr.length);
-    expect(arr)
 
     for(const elem of arr)
         if(elem.id !== id)
             expect(newArr).toContain(elem);
 
     const innerArr = newArr.find(elem => elem.id === id)?.innerArr;
+    expect(innerArr?.length).toBe(arr[0].innerArr.length + 1);
+
     for(const innerElem of arr[0].innerArr)
         expect(innerArr).toContain(innerElem);
 
     expect(innerArr).toContainEqual(newInnerValue);
+});
+
+test("test deleting an inner element", () => {
+    const arr = [
+        {
+            id: "300",
+            innerArr: [
+                {id: "123", value: 400},
+                {id: "3", value: 1400},
+                {id: "4", value: 41100},
+                {id: "5", value: 10}
+            ]
+        },
+        {id: "1231", innerArr: [{id: "444", value: 400}]},
+        {id: "991", innerArr: [{id: "500", value: 400}]}
+    ];
+
+    const id = "300";
+    const innerId = "4";
+    const newArr = dataHelper.deleteInnerElement(
+        arr, id, "innerArr", innerId
+    );
+
+    expect(newArr.length).toBe(arr.length);
+
+    for(const elem of arr)
+        if(elem.id !== id)
+            expect(newArr).toContain(elem);
+
+    const innerArr = newArr.find(elem => elem.id === id)?.innerArr;
+    expect(innerArr?.length).toBe(arr[0].innerArr.length - 1);
+
+    for(const innerElem of arr[0].innerArr)
+        if(innerElem.id !== innerId)
+            expect(innerArr).toContain(innerElem);
+
+    expect(innerArr).not.toContainEqual({id: "4", value: 41100});
+});
+
+test("test editing an inner element", () => {
+    const arr = [
+        {
+            id: "300",
+            innerArr: [
+                {id: "123", value: 400, a: 5},
+                {id: "3", value: 1400, a: 10},
+                {id: "4", value: 41100, a: 300},
+                {id: "5", value: 10, a: 400}
+            ]
+        },
+        {id: "1231", innerArr: [{id: "444", value: 400}]},
+        {id: "991", innerArr: [{id: "500", value: 400}]}
+    ];
+
+    const id = "300";
+    const innerId = "4";
+    const newInnerValue = {value: 500};
+
+    const newArr = dataHelper.editInnerElement(
+        arr, id, "innerArr", innerId, newInnerValue
+    );
+
+    expect(newArr.length).toBe(arr.length);
+
+    for(const elem of arr)
+        if(elem.id !== id)
+            expect(newArr).toContain(elem);
+
+    const innerArr = newArr.find(elem => elem.id === id)?.innerArr;
+    expect(innerArr?.length).toBe(arr[0].innerArr.length);
+
+    for(const innerElem of arr[0].innerArr)
+        if(innerElem.id !== innerId)
+            expect(innerArr).toContain(innerElem);
+
+    expect(innerArr).toContainEqual({id: "4", value: 500, a: 300});
 });
