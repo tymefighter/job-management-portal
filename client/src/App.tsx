ac8b3aa19949f33af3c0bf1baefc9fa51fc299ae
Ahmed Zaheer Dadarkar <ahmed.dadarkar@gmail.com>
2021-07-12 22:41:02 +0530

    Initialize components

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,13 +2,26 @@ import { createStore, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
 import { reducer } from "./redux/reducer";
 import thunk from "redux-thunk";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+
+import Nav from "./Nav";
+import Footer from "./Footer";
 
 const store = createStore(reducer, applyMiddleware(thunk));
 
 function App() {
     return (
         <Provider store={store}>
-            <div></div>
+            <BrowserRouter>
+                <Nav />
+                <Switch>
+                    <Route path="/home"></Route>
+                    <Route path="/companies"></Route>
+                    <Route path="/jobs"></Route>
+                    <Route path="/information"></Route>
+                </Switch>
+                <Footer />
+            </BrowserRouter>
         </Provider>
     );
 }
