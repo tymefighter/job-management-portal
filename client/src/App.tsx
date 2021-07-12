3c166319d161040ed06b0b3f65931f8355267afa
Ahmed Zaheer Dadarkar <ahmed.dadarkar@gmail.com>
2021-07-12 18:48:55 +0530

    Add reducer provider

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,16 @@
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import { reducer } from "./redux/reducer";
+import thunk from "redux-thunk";
+
+const store = createStore(reducer, applyMiddleware(thunk));
+
 function App() {
-    return <div></div>;
+    return (
+        <Provider store={store}>
+            <div></div>
+        </Provider>
+    );
 }
 
 export default App;
