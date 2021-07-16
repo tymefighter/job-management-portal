7f750ce79ccab29a5b47ce822ac9fc00bb552597
Ahmed Zaheer Dadarkar <ahmed.dadarkar@gmail.com>
2021-07-16 10:02:27 +0530

    Add document title test

--- client/src/home/__test__/Home.test.tsx
+++ client/src/home/__test__/Home.test.tsx
@@ -1,8 +1,19 @@
 import { render } from "@testing-library/react";
 import Home from "../Home";
 
+beforeEach(() => {
+    document.title = "";
+})
+
 test("Home snapshot test", () => {
     const {container} = render(<Home />);
 
     expect(container.firstChild).toMatchSnapshot("Home Component Snapshot");
+});
+
+test("Home title test", () => {
+    expect(document.title).toEqual("");
+
+    render(<Home />);
+    expect(document.title).toEqual("Home");
 });
